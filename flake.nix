0ff44ad6613e502bf530295d04f554c0f50f56dd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-15 10:53:36 +0900

    new file:   flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A sample Gtk Rust";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      defaultPackage =
+        with import nixpkgs { system = "${system}"; };
+        stdenv.mkDerivation rec {
+          name = "gtkrs-${version}";
+          pname = "gtkrs";
+          version = "0.2.0-20220215";
+          src = self;
+          buildInputs = rustc.buildInputs ++ [ cargo rustc libiconv openssl pkgconfig ];
+          buildPhase = "cargo build --release";
+        }
+      ;
+    })
+  ;
+}
