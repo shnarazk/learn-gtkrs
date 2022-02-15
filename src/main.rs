456775d1bafc4b6aa9a699027498c485b364f35e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-15 10:53:13 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
-extern crate gtk;
+use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType};
+use gtk::{Button, Window, WindowType, Application, ApplicationWindow};
 
 fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
 
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK+ Program");
-    window.set_default_size(350, 70);
-    let button = Button::with_label("Click me!");
-    window.add(&button);
-    window.show_all();
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
+        let button = Button::with_label("Click me!");
+        window.add(&button);
+        window.show_all();
+
+        window.connect_delete_event(|_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+
+        // Show the window.
+        window.show();
     });
-
-    button.connect_clicked(|_| {
-        println!("Clicked!");
-    });
-
-    gtk::main();
+    app.run();
 }
