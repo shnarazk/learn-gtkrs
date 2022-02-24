69700236c563ec5cc3305ef122385bcfe9d23566
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-24 22:11:07 +0900

    implement increment/decrement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow, Button};
+use {
+    gtk4::prelude::*,
+    gtk4::{Application, ApplicationWindow, Button, Orientation},
+    std::{cell::Cell, rc::Rc},
+};
 
 fn main() {
     let app = Application::builder()
@@ -10,20 +13,42 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Press me!")
+    let button_increase = Button::builder()
+        .label("Increase")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
-    button.connect_clicked(move |button| {
-        button.set_label("Hello World!");
+    let button_decrease = Button::builder()
+        .label("Decrease")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    let number = Rc::new(Cell::new(0i32));
+    let number_inc = number.clone();
+    let number_dec = number;
+    // let number_value = gtk4::Label::builder()
+    //     .label(&format!("{}", number.get()))
+    //     .build();
+    button_increase.connect_clicked(move |_| {
+        number_inc.set(number_inc.get() + 1);
     });
+    button_decrease.connect_clicked(move |_| {
+        number_dec.set(number_dec.get() - 1);
+    });
+    let gtk_box = gtk4::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
+    // gtk_box.append(&number_value);
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("My new GTK App")
-        .child(&button)
+        .title("My GTK App (2nd step)")
+        .child(&gtk_box)
         .build();
     window.present();
 }
