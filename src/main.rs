bfd7ea111dbdfaa4247bcfac9fcd6031ff9580f7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-24 20:00:49 +0900

    add a button

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-// use gtk4 as gtk;
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow};
+use gtk4::{Application, ApplicationWindow, Button};
 
 fn main() {
     let app = Application::builder()
@@ -11,9 +10,20 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    button.connect_clicked(move |button| {
+        button.set_label("Hello World!");
+    });
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My new GTK App")
+        .child(&button)
         .build();
     window.present();
 }
