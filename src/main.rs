f5577e22f4b4896772099a88e6ea43199185b54c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-02-24 19:51:36 +0900

    reset window contents

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
-use gtk4 as gtk;
-use gtk::prelude::*;
-use gtk::{Button, Window, WindowType, Application, ApplicationWindow};
+// use gtk4 as gtk;
+use gtk4::prelude::*;
+use gtk4::{Application, ApplicationWindow};
 
 fn main() {
     let app = Application::builder()
         .application_id("org.example.HelloWorld")
         .build();
-
-    app.connect_activate(|app| {
-        // We create the main window.
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(200)
-            .title("Hello, World!")
-            .build();
-
-        let button = Button::with_label("Click me!");
-        window.add(&button);
-        window.show_all();
-
-        window.connect_delete_event(|_, _| {
-            gtk::main_quit();
-            Inhibit(false)
-        });
-
-        button.connect_clicked(|_| {
-            println!("Clicked!");
-        });
-
-        // Show the window.
-        window.show();
-    });
+    app.connect_activate(build_ui);
     app.run();
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My new GTK App")
+        .build();
+    window.present();
 }
